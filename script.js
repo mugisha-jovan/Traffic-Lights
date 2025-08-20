77c47f3761a4f0bb34769e826fd5add237f63eb3
mugisha-jovan <mugishajovan13@gmail.com>
2025-08-20 16:52:11 +0200

    modify page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const lights = [
     document.getElementById('yellow'),
     document.getElementById('green')
 ];
-const durations = [120000, 6000, 60000]; // red: 120s, yellow: 6s, green: 60s (in ms)
+const durations = [60000, 3000, 30000]; // red: 60s, yellow: 3s, green: 30s (in ms)
 let current = 0;
 
 function showLight(index) {
